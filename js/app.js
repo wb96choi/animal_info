87aa77c5db6ab5472619f566a702bdbce9598333
1_b1n <choi44601280@gmail.com>
2022-07-04 10:21:40 +0900

    app.js 수정

--- js/app.js
+++ js/app.js
@@ -6,12 +6,33 @@ fetch('https://raw.githubusercontent.com/csslick/animal-mobile/main/animal-data.
     .then(function(res){
         return res.json(); // JSON 객체 변환
     })                
-    .then(function(data){
+    .then(function(obj){
         // 최종 데이터 출력(object)
-        console.log(data);
-        let name = data[0].name;
-        let category = data[0].category;
-        console.log(`name = ${name}`);
-        console.log(`category = ${category}`);
+        // console.log(obj);
+        let name = obj[0].name;
+        let category = obj[0].category;
+        let imgUrl = obj[0].imgUrl;
 
+        // console.log(`name = ${name}`);
+        // console.log(`category = ${category}`);
+
+        for(let i = 0; i < obj.length; i++) {
+            let html = `
+                <div class="col">
+                    <img src=${obj[i].imgUrl} alt="dog01">
+                    <p class="name">${obj[i].name}</p>
+                </div>    
+            `
+            $('.row').append(html);
+        }
+
+        
+    
+    
+        // forEach() 반복문
+        obj.forEach(function(animal){
+            console.log(animal.name)
+        });
+    
+    
     });
